6243923f867b0eda4e3d9009e10d7389a7a550c4
gguidoxx <gudombe@gmail.com>
2023-07-29 15:07:08 -0300

    Whatsapp button, scroll to top button, AboutUs y Footer creados

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { NavBar } from "./components/NavBar/NavBar";
 import { MainPage } from "./components/MainPage/MainPage";
 import { Footer } from "./components/Footer/Footer";
-import AboutUs from "./components/AboutUs/AboutUS";
-import { WhatsAppButton } from "./components/WhatsappButton/WhatsappButton";
 
 function App() {
   return (
