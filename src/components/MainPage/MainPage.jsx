289290adb68bbd55baddcbc38efafff284c1351f
gguidoxx <gudombe@gmail.com>
2023-07-29 15:33:00 -0300

    Cambios deploy

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { WhatsAppButton } from "../WhatsappButton/WhatsappButton";
 import { ScrollToTopButton } from "../ScrollToTop/ScrollToTop";
-import { AboutUs } from "../AboutUs/AboutUS.jsx";
+import { AboutUs } from "../AboutUs/AboutUS";
 
 export const MainPage = () => {
   return (
     <>
-      <AboutUs />
-      <AboutUs />
       <AboutUs />
       <WhatsAppButton />
       <ScrollToTopButton />
